6949f3bfdfedf7aed76984ea77c50f31108fbc9a
devin <raaf7763@gmail.com>
2025-09-07 13:39:53 +0700

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [NavbarComponent,FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
